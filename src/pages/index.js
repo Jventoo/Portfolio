6118abbf3758a1646fe54579113ddaae5a948165
Jack Vento <jackovento@gmail.com>
2024-01-26 14:16:36 -0800

    Define types

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,17 @@ const StyledMainContainer = styled.main`
   counter-reset: section;
 `;
 
+const StyledTitle = styled.h1`
+  color: var(--green);
+  font-family: var(--font-mono);
+  font-size: clamp(100px, 25vw, 200px);
+  line-height: 1;
+`;
+const StyledSubtitle = styled.h2`
+  font-size: clamp(30px, 5vw, 50px);
+  font-weight: 400;
+`;
+
 const maintenanceContent = (
   <StyledMainContainer className="fillHeight">
     <StyledTitle>Site under maintenance</StyledTitle>
